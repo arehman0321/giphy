0a92d8b76ea8b9b33f7a74c1930de7482a57705b
abdul rehman <arehman@aoiths.org>
2019-04-15 00:18:07 +0000

    js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,29 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
+var userIpt,
+    giphy_url,
+    a,
+    num,
+    source,
+    source2;
 
-$("#search-button").click(function(){
-  
-  
-  
+
+$("button").click(function(){
+    userIpt = $("#search-term").val();
+    giphy_url = "https://api.giphy.com/v1/gifs/search?q=" + userIpt + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+        url: giphy_url,
+        method: "GET",
+        success: function(response) {
+            a = response["data"].length;
+            num = Math.floor(Math.random() * a);
+            source = response["data"][num].images.original.url;
+            source2 = response["data"][num].url;
+            $("h3").html("<a href='" + source2 +"' target='_blank'><img src='" + source + "'></a>");
+        }
+    });
 });
+
+
 
